32b86f853d2dfd12a2c0b4577aaa404e84b457ea
Stephen Nash <stephen.aa.nash@gmail.com>
2023-08-27 16:44:44 -0400

    Add Booleans variables

--- varibles.rb
+++ varibles.rb
@@ -11,3 +11,9 @@ year_synthesized = 1902
 specific_gravity_avg = 4.01
 refractive_index_avg = 1.77
 birefringence = -0.008
+
+### Booleans
+rubies_red = true
+violets_blue = true
+onions_stink = true
+you_stink = false
