f71fe91d915081b242ac14e6a8e2885d8974f678
Stephen Nash <stephen.aa.nash@gmail.com>
2023-08-28 09:59:48 -0400

    Reassign values from each category and add puts statements

--- varibles.rb
+++ varibles.rb
@@ -17,3 +17,19 @@ rubies_red = true
 sapphires_blue = true
 onions_stink = true
 you_stink = false
+
+# Reassign color, which cascades info changes
+
+color = "blue"
+name = "sapphire"
+year_synthesized = 1873
+specific_gravity_avg = 3.99
+you_stink = true
+
+# Add several puts statements
+
+puts name
+puts color
+puts year_synthesized
+puts specific_gravity_avg
+puts you_stink
