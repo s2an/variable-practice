5c7d9e70e057a7c4e38950cd4d5533bbc1bc17ff
Stephen Nash <stephen.aa.nash@gmail.com>
2023-08-28 09:54:09 -0400

    Edit headers format and line 17

--- varibles.rb
+++ varibles.rb
@@ -1,19 +1,19 @@
-### Strings
+# Strings
 name = "ruby"
 mineral_type = "Corundum"
 color = "red"
 
-### Integers
+# Integers
 hardness = 9
 year_synthesized = 1902
 
-### Floats
+# Floats
 specific_gravity_avg = 4.01
 refractive_index_avg = 1.77
 birefringence = -0.008
 
-### Booleans
+# Booleans
 rubies_red = true
-violets_blue = true
+sapphires_blue = true
 onions_stink = true
 you_stink = false
